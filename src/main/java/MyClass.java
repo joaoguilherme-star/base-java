9c67190834226667f0d79ffc2c0967567f3219bc
joaoguilherme <joao.oliveira0@outlook.com>
2025-03-12 00:35:58 -0300

    up studies

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main (String [] args){
+        String Name = "João Guilherme";
+        int Age = 23;
+        boolean isMale = true;
+
+        System.out.print("My name is " + Name + " ,i am " + Age + " years old and i'm " + isMale);
+    }
+}
